6081b792aab543d93159358af589b9abe165cf0e
ORIENTSOFTWARE\tttu <tu.tran@orientsoftware.net>
2017-01-19 15:43:56 +0700

    Binding

--- src/userService.ts
+++ src/userService.ts
@@ -2,9 +2,9 @@ import {IUserService} from "./iuserService";
 export class UserService implements IUserService {
     public getUsers(): Array<any> {
         let users: Array<any> = [
-            { id: 1, firstName: "Tu", lastName: "Tran", userName: "tutran" },
-            { id: 2, firstName: "Tu 1", lastName: "Tran", userName: "tutran2" },
-            { id: 3, firstName: "Tu 2", lastName: "Tran", userName: "tutran3" }
+            { id: 1, firstName: "Tu", lastName: "Tran", userName: "tutran", color:"red" },
+            { id: 2, firstName: "Tu 1", lastName: "Tran", userName: "tutran2" , color:"blue"},
+            { id: 3, firstName: "Tu 2", lastName: "Tran", userName: "tutran3" , color:"green"}
         ];
         return users;
     }
--- src/users.ts
+++ src/users.ts
@@ -3,11 +3,16 @@ import { Router } from "@angular/router";
 import { UserService } from "./userService";
 @Component({
     selector: "users",
-    templateUrl: "src/users.html"
+    templateUrl: "src/users.html",
+    styles: ['table th{font-size: 20px;}']
 })
 export class Users {
     private users: Array<any> = [];
+    public currentSelected: any = null;
     constructor(userService: UserService) {
         this.users = userService.getUsers();
+    }
+    public onPreviewClicked(user: any) {
+        this.currentSelected = user;
     }
 }
